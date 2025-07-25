697bacd757f464b3d2ede26aac3b26c2b88cd97c
chetanCronbay <chetan@cronbay-tech.com>
2025-07-25 16:44:42 +0530

    p

--- products/models.py
+++ products/models.py
@@ -15,16 +15,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 def category_image_upload_path(instance, filename):
-    return f'category/{instance.name}/{filename}'
+    return f'category/{instance.id}/{filename}'
 
 def subcategory_image_upload_path(instance, filename):
-    return f'subcategory/{instance.name}/{filename}'
+    return f'subcategory/{instance.id}/{filename}'
 
 def product_image_upload_path(instance, filename):
-    return f'products/{instance.product.name}/{filename}'
+    return f'products/{instance.product.id}/{filename}'
 
 def product_brochure_upload_path(instance, filename):
-    return f'products/{instance.name}/brochure/{filename}'
+    return f'products/{instance.id}/brochure/{filename}'
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
