d7a5bce0d9c1bd91d4b8e8edf0a546adf4705c6b
chetanCronbay <chetan@cronbay-tech.com>
2025-08-01 10:46:54 +0530

    changes

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from products.models import Category 
 
+def blog_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f'user_banner{instance.user.id}/{filename}'
+
 class Blog(models.Model):
     blog_title = models.CharField(max_length=100)
     blog_category = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='blogCategory', help_text="Blog category")
-    image1 = models.CharField(max_length=500, null=False, blank=False)
+    image1 = models.ImageField(max_length=500, null=False, blank=False, upload_to=blog_path)
     description = models.TextField(null=True, blank=True)
     meta_title = models.CharField(max_length=200, null=True, blank=True)
     description1 = models.CharField(max_length=500, null=True, blank=True)
