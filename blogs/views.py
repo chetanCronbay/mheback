585d0a809d5c3274f782667ac2bac97a258679ac
chetanCronbay <chetan@cronbay-tech.com>
2025-08-01 11:27:31 +0530

    blog unauth user read

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Blog
 from .serializers import BlogSerializer
 
 class BlogViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for blogs.
+    - Public can read (list and detail).
+    - Authenticated users can create, update, and delete.
+    """
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
